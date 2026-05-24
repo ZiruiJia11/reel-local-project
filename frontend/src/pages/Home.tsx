96cbe365b203c4c519040b3d978b55dd38f63d1a
Zirui Jia <steven5115115@gmail.com>
2026-05-24 22:42:08 +1200

    update home and movies page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import { useNavigate } from "react-router-dom"
+
 function Home() {
-  return <h1>Home</h1>
+
+  const navigate = useNavigate()
+
+  return (
+    <div className="hero">
+
+      <h1>
+        Reel Local Cinema
+      </h1>
+
+      <p>
+        Discover local screenings,
+        reserve your seat,
+        and connect with movie lovers.
+      </p>
+
+      <button
+        onClick={() => navigate("/movies")}
+      >
+        Browse Movies
+      </button>
+
+    </div>
+  )
 }
 
 export default Home
--- frontend/src/pages/Movies.tsx
+++ frontend/src/pages/Movies.tsx
@@ -1,5 +1,57 @@
+const movies = [
+  {
+    title: "Citizen Kane",
+    image: "/movies/citizen_kane.jpg",
+  },
+
+  {
+    title: "The Princess Bride",
+    image: "/movies/princess_bride.jpg",
+  },
+
+  {
+    title: "Pulp Fiction",
+    image: "/movies/pulpfiction.jpg",
+  },
+
+  {
+    title: "The Shawshank Redemption",
+    image: "/movies/shawshank.jpg",
+  },
+
+  {
+    title: "The Third Man",
+    image: "/movies/third_man.jpg",
+  },
+]
+
 function Movies() {
-  return <h1>Movies</h1>
+  return (
+    <div className="movie-grid">
+
+      {
+        movies.map(movie => (
+
+          <div
+            className="movie-card"
+            key={movie.title}
+          >
+
+            <img
+              src={movie.image}
+            />
+
+            <h2>
+              {movie.title}
+            </h2>
+
+          </div>
+
+        ))
+      }
+
+    </div>
+  )
 }
 
 export default Movies
