bdf56c97f220109a94bf972c080ce69dd0f20702
Zirui Jia <steven5115115@gmail.com>
2026-05-24 14:46:38 +1200

    made the frame work for diff pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Movies from "./pages/Movies"
+import Schedule from "./pages/Schedule"
+import Profile from "./pages/Profile"
+
 function App() {
   return (
-    <>
-      <h1>Reel Local Cinema</h1>
-      <p>Project Initialization Complete</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
